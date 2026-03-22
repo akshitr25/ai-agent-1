987ee5f32e10e6d7bce8204505aa3b8c4a10a151
Akshit Rathod <akshitrathod1025@gmail.com>
2026-03-23 00:57:18 +0530

    cors error fix trial 1

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from agent import get_response
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all (fine for now)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
